5b7895acaf65ab56adb91d49eaf53c5629e2d017
Matt Peperell <matt.peperell@digital.justice.gov.uk>
2017-06-05 11:20:05 +0100

    Refactor for rubocop
    We set TargetRubyVersion because the <<~ ruby operator for heredocs
    (which ignores leading whitespace) has a minimum ruby version
    requirement.

--- web.rb
+++ web.rb
@@ -1,14 +1,82 @@
 require 'sinatra/base'
 require 'github_api'
 
+# Main bot is in this class
 class WebListener < Sinatra::Base
-  DEFAULT_REPOS = %w( bootstrap-cfn bootstrap-salt template-deploy ).freeze
+  attr_accessor :github
+
+  DEFAULT_REPOS = %w[bootstrap-cfn bootstrap-salt template-deploy].freeze
+
+  def get_team_id(name)
+    all_teams = @github.orgs.teams.list org: 'ministryofjustice'
+    all_teams.select { |t| t.name == name }.first.id
+  end
+
+  def read_team_prs(team)
+    team_id = get_team_id team
+    repos = @github.orgs.teams.list_repos team_id
+    repos.map { |repo| read_prs_for_repo repo.name }.flatten
+  end
+
+  def read_default_repos
+    DEFAULT_REPOS.map do |repo|
+      read_prs_for_repo repo
+    end.flatten
+  end
+
+  def usage
+    usages = [
+      'open prs',
+      'open prs for team &lt;team&gt;',
+      'open prs in repo &lt;repo&gt;'
+    ]
+    body %({"text": "Say one of the following: #{usages.join(',')}"})
+  end
+
+  def pr_title(pr)
+    "#{pr.base.repo.name}/#{pr.number}: #{pr.title}"
+  end
+
+  def format_pr(pr)
+    if pr.assignee
+      "• #{pr_title(pr)} by #{pr.user.login}, assigned to #{pr.assignee.login}: #{pr.html_url}"
+
+    else
+      "• #{pr_title(pr)} by #{pr.user.login}: #{pr.html_url}"
+    end
+  end
 
   get '/' do
     <<~EOT
       This is the webapp which is used as a Slack webhook to notify channels
       about open pull requests on select repos on github.
     EOT
+  end
+
+  def read_prs_for_repo(repo)
+    this_repo_prs = @github.pull_requests.list ENV['GH_ORG'], repo
+    this_repo_prs.sort_by(&:number)
+  end
+
+  def read_prs_for_message(text)
+    if text =~ /for team (.*)/
+      read_team_prs Regexp.last_match[1]
+    elsif text =~ /in repo (.*)/
+      read_prs_for_repo Regexp.last_match[1]
+    elsif text =~ /help/
+      usage
+    else
+      read_default_repos
+    end
+  end
+
+  def compose_response(messages)
+    if messages.any?
+      response = "The following PRs are open:\n" + messages.join("\n")
+      '{ "text": "' + response.gsub('"', '\"') + '"}'
+    else
+      '{ "text": "No pull requests found" }'
+    end
   end
 
   post '/webhook' do
@@ -24,54 +92,20 @@ class WebListener < Sinatra::Base
       break
     end
     begin
-      # TODO: check 'repos' or 'teams' in the message
-      github = Github.new basic_auth: "#{ENV['GH_USER']}:#{ENV['GH_TOKEN']}", auto_pagination: true
-      prs = []
+      @github = Github.new basic_auth: "#{ENV['GH_USER']}:#{ENV['GH_TOKEN']}", auto_pagination: true
 
-      if params[:text] =~ /for team (.*)/
-        all_teams = github.orgs.teams.list org: 'ministryofjustice'
-        team_id = all_teams.select{|t| t.name == $1 }.first.id
-        repos = github.orgs.teams.list_repos team_id
-        repos.each do |repo|
-          this_repo_prs = github.pull_requests.list ENV['GH_ORG'], repo.name
-          prs += this_repo_prs.sort_by(&:number)
-        end
-      elsif params[:text] =~ /in repo (.*)/
-          this_repo_prs = github.pull_requests.list ENV['GH_ORG'], $1
-          prs += this_repo_prs.sort_by(&:number)
-      elsif params[:text] =~ /help/
-        body '{"text": "Say one of the following: pulls, pulls for team &lt;team&gt;, pulls in repo &lt;repo&gt;"}'
-        return
-      else
-        DEFAULT_REPOS.each do |repo|
-          this_repo_prs = github.pull_requests.list ENV['GH_ORG'], repo
-          prs += this_repo_prs.sort_by(&:number)
-        end
-      end
+      prs = read_prs_for_message(params[:text])
+      break unless prs
 
-      messages = prs.map do |pr|
-        if pr.assignee
-          "• #{pr.base.repo.name}/#{pr.number}: *#{pr.title}* " \
-            "by #{pr.user.login}, assigned to #{pr.assignee.login}: #{pr.html_url}"
-
-        else
-          "• #{pr.base.repo.name}/#{pr.number}: *#{pr.title}* " \
-            "by #{pr.user.login}: #{pr.html_url}"
-        end
-      end
-      if messages.any?
-        response = "The following PRs are open:\n" + messages.join("\n")
-        body '{ "text": "' + response.gsub('"', '\"') + '"}'
-      else
-        body '{ "text": "No pull requests found" }'
-      end
+      formatted_prs = prs.map { |pr| format_pr(pr) }
+      body compose_response formatted_prs
     rescue Github::Error::Unauthorized
       status 500
       body '{ "error": { "code": 500, "message": "Could not authenticate with github." } }'
     rescue Github::Error::NotFound
       status 500
-      body '{ "error": { "code": 500, "message": "Github did not understand our request. Perhaps GH_ORG is wrong?" } }'
+      message = 'Github did not understand our request. Perhaps GH_ORG is wrong?'
+      body %({ "error": { "code": 500, "message": #{message} } })
     end
   end
 end
-
